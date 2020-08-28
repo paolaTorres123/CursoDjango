d14bdb7a733fdd807c83b8e0170e6e46a20f77ba
paolatorres <pao.paotores18@gmail.com>
2020-08-27 19:27:48 -0500

    Creando los templates

--- proyecto1/core/views.py
+++ proyecto1/core/views.py
@@ -1,37 +1,19 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-<h1>Mi Web Personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="about">Acerca de </a></li>
-    <li><a href="portfolio">Portafolio</a></li>
-    <li><a href="contact">Contacto</a></li>
-</ul>
-"""
+
 
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esta es la portada</p>
-    """)
+    return render(request, "core/home.html")
+
+def home(request):
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse("""
-        <h1>Mi WEB personal</h1>
-        <h2>Acerca de</h2>
-        <p>Me llamo paola torres macias</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse("""
-        <h2>Portafolio</h2>
-        <p>Este es el portafolio</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse("""
-        <h2>Contacto</h2>
-        <p>Me llamo paola torres macias</br> 4851294408</p>
-    """)
+    return render(request, "core/contact.html")

8d9a75040377ea3d3c5c3cf711fd48d90eb5bf97
paolatorres <pao.paotores18@gmail.com>
2020-09-03 21:23:36 -0500

    Agregando link

--- proyecto1/portfolio/models.py
+++ proyecto1/portfolio/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to = "projects")
+    link = models.URLField(null=True, blank=True, verbose_name="Direccion Web")
     created = models.DateTimeField(auto_now_add =True, verbose_name="Fecha de creacion")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edicion")
 

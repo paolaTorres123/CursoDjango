0af272b78f9f620811371e856ddcba37d88d26a7
paolatorres <pao.paotores18@gmail.com>
2020-09-29 17:44:52 -0500

    Vistas CRUD con CBV:CreateView

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PageDetailView, PageListView
+from .views import PageDetailView, PageListView, PageCreate
 
-urlpatterns = [
+pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
-]
+    path('create/',PageCreate.as_view(), name='create'),
+    ],'pages')
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,8 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class PageListView (ListView):
@@ -8,3 +10,8 @@ class PageListView (ListView):
 
 class PageDetailView (DetailView):
     model = Page
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ['title','content','order']
+    success_url = reverse_lazy('pages:pages')

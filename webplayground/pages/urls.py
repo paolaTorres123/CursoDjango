82a3f429cfcfd11e8e3368178cce89312176c2a9
paolatorres <pao.paotores18@gmail.com>
2020-10-06 18:52:24 -0500

    Vistas Updateview y Deleteview

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PageDetailView, PageListView, PageCreate
+from .views import PageDetailView, PageListView, PageCreate, PageUpdate, PageDelete
 
 pages_patterns = ([
     path('', PageListView.as_view(), name='pages'),
     path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('create/',PageCreate.as_view(), name='create'),
+    path('update/<int:pk>',PageUpdate.as_view(), name='update'),
+    path('delete/<int:pk>',PageDelete.as_view(), name='delete'),
     ],'pages')
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,7 +1,7 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -14,4 +14,18 @@ class PageDetailView (DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['title','content','order']
+    success_url = reverse_lazy('pages:pages')
+
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title','content','order']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('pages:pages')
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args = [self.object.id]) + '?ok'
+
+
+class PageDelete(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')

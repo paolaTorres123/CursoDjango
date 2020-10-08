a21ec3b5895ca9d2b8ba9f4c11e67f777ac7b924
paolatorres <pao.paotores18@gmail.com>
2020-10-07 21:15:41 -0500

    terminando pages creando registration

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -6,14 +6,14 @@ from django.urls import reverse_lazy
 from .models import Page
 from .forms import PageForm
 from django.shortcuts import redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 class StaffRequiredMixin(object):
     #Este mixin requeriria que el usuario sea miebro del staff
-
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
         return super(StaffRequiredMixin, self).dispatch(request,*args,**kwargs)
 
 
@@ -23,12 +23,14 @@ class PageListView (ListView):
 class PageDetailView (DetailView):
     model = Page
 
-class PageCreate(StaffRequiredMixin,CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
-class PageUpdate(StaffRequiredMixin,UpdateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -36,7 +38,7 @@ class PageUpdate(StaffRequiredMixin,UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args = [self.object.id]) + '?ok'
 
-
-class PageDelete(StaffRequiredMixin,DeleteView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
     path('admin/', admin.site.urls),
+    #Paths Autho 
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

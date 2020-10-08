c5338279c5bbdf05387ee4d37885f91a3d583872
paolatorres <pao.paotores18@gmail.com>
2020-10-07 19:01:19 -0500

    Creando un Mixin de identificacion

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -5,20 +5,30 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Page
 from .forms import PageForm
+from django.shortcuts import redirect
 # Create your views here.
+
+class StaffRequiredMixin(object):
+    #Este mixin requeriria que el usuario sea miebro del staff
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request,*args,**kwargs)
+
+
 class PageListView (ListView):
     model = Page
 
 class PageDetailView (DetailView):
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin,CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
-
-class PageUpdate(UpdateView):
+class PageUpdate(StaffRequiredMixin,UpdateView):
     model = Page
     form_class = PageForm
     template_name_suffix = '_update_form'
@@ -27,6 +37,6 @@ class PageUpdate(UpdateView):
         return reverse_lazy('pages:update', args = [self.object.id]) + '?ok'
 
 
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin,DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')

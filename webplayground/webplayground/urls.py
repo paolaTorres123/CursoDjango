b1d2debe887df77ceb97ec1969e2de8e595e9c11
paolatorres <pao.paotores18@gmail.com>
2020-10-19 23:56:05 -0500

    Registro con CBV

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Paths Autho 
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
